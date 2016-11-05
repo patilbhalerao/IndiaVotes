8605c79acb8966c494b2f31176954d6e5d9986c0
SAIBABA <SAIBABA>
2016-11-05 13:06:00 +0530

    IndiaVotes

---
+++ PageObject/pageobjects/Home.java
@@ -0,0 +1,26 @@
+package pageobjects;
+
+public class Home {
+	
+	String xpathTabHomeLink="//li/a[@title="Home"]";
+	String xpathTabAboutUsLink=".//*[@id='dd_menu']/li[2]/a";
+	String xpathTabSearchFilterLink=".//*[@id='dd_menu']/li[2]/a";
+	String xpathTabElectionMapsLink=".//*[@id='dd_menu']/li[4]/a";
+	String xpathTabSummaryReportLink=".//*[@id='dd_menu']/li[5]/a";
+	String xpathTabBlogLink=".//*[@id='dd_menu']/li[6]/a"]";
+	String xpathTabReportErrorLink=".//*[@id='dd_menu']/li[7]/a"]";
+	
+	
+	String cssPCElectionYear="#year";
+	String cssPCElectionState="#state";
+	String cssACElectionState="#stateac";
+	String cssACElectionYear="#yearac";
+			
+	String cssPartName="#partylist";
+	String cssSearch=".searchIcon.fr.nth-child(2)";
+	
+	String xpathTabPartNameLink=".//*[@id='partylist']";
+	String xpathTabPartySearchLink=".//*[@id='form']/input[2]";
+	String xpathACElectionGoButton=".//select[@id='yearac']following-sibling::input";"
+	
+}
